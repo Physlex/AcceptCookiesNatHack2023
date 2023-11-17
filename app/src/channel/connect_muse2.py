97fc50c4c5cce860754e69e163ea6081959b754b
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-11-17 11:09:27 -0700

    Set up basic templating for html website

---
+++ app/src/channel/connect_muse2.py
@@ -0,0 +1,25 @@
+import time
+from brainflow.board_shim import (
+    BoardShim,
+    BrainFlowInputParams,
+    BoardIds,
+)
+
+def connect_brainflow():
+    """
+    Connects with MUSE2 using brainflow and returns the buffer
+    """
+    BoardShim.enable_dev_board_logger()
+
+    params = BrainFlowInputParams()
+    params.serial_port = "4"
+
+    board = BoardShim(BoardIds.MUSE_2_BOARD, params)
+    board.prepare_session()
+    board.start_stream()
+    time.sleep(2)
+
+    board_data_buff = board.get_board_data(256)
+
+    board.release_session()
+    return board_data_buff
