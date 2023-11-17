7cbf35431966f6f9c352b59b7a40e9c386942f13
unknown <al.willet02@gmail.com>
2023-11-17 11:12:55 -0700

    altering eeg tests

--- app/src/channel/test.py
+++ app/src/channel/test.py
@@ -1,6 +1,7 @@
 import argparse, time
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
 import numpy as np
+import matplotlib.pyplot as plt
 
 def run_tests() -> None:
     """
@@ -12,15 +13,22 @@ def run_tests() -> None:
     params = BrainFlowInputParams()
     params.serial_port = "4"
 
+    # Call stream
     board = BoardShim(BoardIds.MUSE_2_BOARD, params)
     board.prepare_session()
     board.start_stream()
     time.sleep(2)
-
-    board_data_buff = board.get_board_data(256)
-
+    board_data_buff = board.get_board_data(num_samples=None, preset=BrainFlowPresets.DEFAULT_PRESET)
+    board_eeg_chann = board.get_eeg_channels(board_id=BoardIds.MUSE_2_BOARD, preset=BrainFlowPresets.DEFAULT_PRESET)
     board.release_session()
-    print(board_data_buff)
+
+    # Output brain data
+    print("Standard board data")
+    for i in range(len(board_data_buff)):
+        print(board_data_buff[i])
+
+    print("EEG - specific data")
+    print(board_eeg_chann)
 
     return
 
