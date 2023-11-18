aa30462cddcfab9bf62ea82c63019758549c9561
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-11-17 17:30:08 -0700

    Got the button working

--- app/src/channel/app.py
+++ app/src/channel/app.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from pathlib import Path
 from connect_muse import connect_brainflow
 import uvicorn
+from models import User,Data
 import mongoengine
 
 app = FastAPI()
 mongoengine.connect("NatHacks")
+
+origins = ["localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mount the 'templates' folder to serve HTML files
 app.mount("/templates", StaticFiles(directory="templates"), name="templates")
@@ -40,8 +53,23 @@ async def read_html(filename: str):
 
 
 @app.post("/connect_brainflow")
-async def connect():
-    return connect_brainflow()
+async def connect(id:int = 5):
+    brainflow_data = connect_brainflow(id)
+    eeg_channels = brainflow_data["board_egg_chann"]
+    timestamp_channel = brainflow_data["board_time_chann"]
+    user = User.objects().first()
+    data = Data(
+        timestamps = brainflow_data["board_data_buff"][timestamp_channel],
+        data = [brainflow_data["board_data_buff"][i] for i in eeg_channels],
+        channels = eeg_channels,
+        device_used = "muse2"
+    )
+    data = user.data.append(data)
+    user.update(
+        set__data = data
+    )
+    return JSONResponse(content={"Succes":"True!"})
+    
 
 
 if __name__ == "__main__":
