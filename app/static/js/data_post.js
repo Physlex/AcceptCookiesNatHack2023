bd72116efb2ddf1fea1e4060f0c07846de1ddc9d
unknown <al.willet02@gmail.com>
2023-11-20 00:22:24 -0700

    cleaned up UI a bit

--- app/static/js/data_post.js
+++ app/static/js/data_post.js
@@ -1,6 +1,6 @@
 const connect_url = 'http://localhost:8000/connect_brainflow';
 document.querySelector('#start-btn').addEventListener('click', () => {
-    let payload = document.querySelector('#start-text-field').value;
+    let payload = document.querySelector('#port-text-field').value;
     console.log(payload);
 
     // Post the payload to the muse board
@@ -14,7 +14,7 @@ document.querySelector('#start-btn').addEventListener('click', () => {
 
 const resolve_url = 'http://localhost:8000/remove_connection';
 document.querySelector('#stop-btn').addEventListener('click', () => {
-    let payload = document.querySelector('#stop-text-field').value;
+    let payload = document.querySelector('#port-text-field').value;
     console.log(payload);
     // Post the payload to the muse board
     fetch(resolve_url+"?id="+payload, {
