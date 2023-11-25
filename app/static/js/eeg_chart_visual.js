44dce20aea6d8492e0ba3d2c3eee254363cff663
Physlex <al.willet02@gmail.com>
2023-11-24 22:40:31 -0700

    created chart class

--- app/static/js/eeg_chart_visual.js
+++ app/static/js/eeg_chart_visual.js
@@ -1,14 +1,98 @@
-const num_seconds = 1; // You can change this
-const SEC_TO_MILLISEC = 1000;
-window.setInterval(shortPollEEG, (num_seconds * SEC_TO_MILLISEC));
+/// CLASSES
 
+/**
+ *
+*/
+class EEGChart {
+  /// PUBLIC
+
+  /**
+   * @brief Turns a data package into a chart for eeg-data-analysis.
+   * 
+   * @details It is required that data_package is a hash table with the
+   *          parameters 'eeg_channels' which holds the eeg_channel data
+   *          and 'timestamp_channel' which holds the times at each
+   *          eeg data sample instance.
+   * 
+   * @param data_package, map:
+   *            A fetched package with eeg_channel data in
+   *            the 'eeg_channels' access, and timestamps for sampled
+   *            eeg data in the 'timestamp_channel' access.
+   * @param type, str:
+   *            The type of chart. Default is line chart.
+   * @param html_ctx, str:
+   *            Should be in the form of a css selector for an id.
+   *            The ID should be for the canvas element assigned
+   *            to hold the chart.
+  */
+  constructor(data_package, type='line', html_ctx='#eeg-data-visual') {
+    this.eeg_channels = data_package["eeg_channels"];
+    if(this.eeg_channels == undefined) {
+      throw ("eeg_channels is undefined. Likely a network or response error\n");
+    }
+
+    this.timestamp_channel = data_package["timestamp_channel"];
+    if(this.timestamp_channel == undefined) {
+      throw ("timestamp_channel is undefined. Likely a network or response error\n");
+    }
+
+    this.type = type;
+    this.context_id = html_ctx;
+  }
+  
+  /**
+   * 
+  */
+  updateChart() {
+    // TODO: FINISH FUNCT
+    console.log("Called!");
+  }
+
+
+  /// PRIVATE
+
+  /**
+   * @brief Turns the context, type, and datasets allocated in constructor
+   *        into a canvas chart.
+   * 
+   * @details asynchronous. Returns a response object that must be handled.
+   *          Type of returned object is a chart object from chartjs.
+   * 
+   * @return chart, promise(Chart):
+   *         Returns a chart promise to be resolved on return.
+  */
+  async constructChart() {
+    let datasets = [];
+    for (let i = 0; i < this.eeg_channels.length; ++i) {
+      datasets.push({label: `Channel ${i + 1}`, data: this.eeg_channels[i]});
+    }
+
+    const epoch = this.timestamp_channel[0];
+    for (let i = 0; i < this.timestamp_channel.length; i++) {
+      this.timestamp_channel[i] = (this.timestamp_channel[i] - epoch).toFixed(4);
+    }
+    const time_series_data = {datasets: datasets, labels: this.timestamp_channel};
+
+    const config = {type: this.type, data: time_series_data};
+    const context = document.querySelector(this.context_id);
+    this.chart_internal = await new Chart(context, config);
+  }
+}
+
+
+/// FUNCTIONS
+
+/**
+ * 
+ * @returns 
+ */
 async function fetchEEGData() {
   const data_url = 'http://localhost:8000/poll_data';
   const response = await fetch(data_url, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }).then((packet) => {
     return packet.json();
   });
@@ -16,39 +100,21 @@ async function fetchEEGData() {
   return response;
 }
 
-
-/// CHART FUNCTIONALITY
-
-// Configures and updates the global chart context
-async function createEEGChart(chart_type, eeg_channels, timestamps) {
-  let datasets = [];
-  for (i = 0; i < eeg_channels.length; ++i) {
-    datasets.push({label: `Channel ${i + 1}`, data: eeg_channels[i]});
-  }
-  const time_series_data = {datasets: datasets, labels: timestamps};
-
-  const context = document.querySelector('#eeg-data-visual');
-  const config = {type: chart_type, data: time_series_data};
-  console.log(config);
-  let eeg_chart = await new Chart(context, config);
-
-  return eeg_chart;
+function shortPollUpdateChart(chart) {
+  chart.updateChart();
 }
 
-function updateChart(new_data, label) {
-  // TODO: FINISH FUNCT
-}
 
-function shortPollEEG() {
-  fetchEEGData().then( (data_package) => {
-    let eeg_channels = data_package["eeg_channels"];
-    let timestamp_channel = data_package["timestamp_channel"];
+/// 'MAIN'
 
-    const epoch = timestamp_channel[0];
-    for (i = 0; i < timestamp_channel.length; i++) {
-      timestamp_channel[i] = timestamp_channel[i] - epoch;
-    }
+let eeg_data_visual_chart = fetchEEGData().then((data_packet) => {
+  let eeg_data_visual_chart = new EEGChart(data_packet);
+  eeg_data_visual_chart.constructChart();
+  return eeg_data_visual_chart;
+});
 
-    createEEGChart('line', eeg_channels, timestamp_channel);
-  });
-}
+const num_seconds = 1;
+const SEC_TO_MILLISEC = 1000;
+eeg_data_visual_chart.then((chart) => {
+  window.setInterval(shortPollUpdateChart, (num_seconds * SEC_TO_MILLISEC), chart);  
+})
